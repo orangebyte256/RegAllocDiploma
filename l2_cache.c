aeb85c3a03967736a504cecb31839ccc0c26a5ff
orangebyte256 <dmitry.morsky@gmail.com>
2016-12-06 00:42:18 +0700

    upgrade case

--- l2_cache.c
+++ l2_cache.c
@@ -1,48 +1,64 @@
-#define POS_LENGTH 10000000
-#define L1_SIZE_LINE 4096
-#define REG_COUNT_TEST 8
+#include "def.h"
+#include <stdio.h>
+#define N 10000000
 
-int foo(unsigned int *__restrict__ pos, unsigned int *__restrict__ values)
+void clear();
+
+static __inline__ unsigned long long rdtsc(void)
+{
+    unsigned long long int x;
+    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+    return x;
+}
+
+//int foo(unsigned int *__restrict__ pos, unsigned int *__restrict__ values)
+int foo()//unsigned int *__restrict__ pos)
 {
 	unsigned int arr[L1_SIZE_LINE * REG_COUNT_TEST];	
+	clear();
+	for(int i = 0; i < L1_SIZE_LINE * REG_COUNT_TEST; i++) {
+		arr[i] = i + arr[i - 1];
+	}
+	unsigned long long start = rdtsc();
 	for(int i = 0; i < POS_LENGTH; i++) {
-//		int val = *(pos++) % REG_COUNT_TEST;
+//		int val = *(pos++);
+//		arr[(i % REG_COUNT_TEST) * L1_SIZE_LINE] += *(values++);
 		switch(i % REG_COUNT_TEST) {
 		case 0:
-			arr[0 * L1_SIZE_LINE] += 7;	
+			arr[0 * L1_SIZE_LINE] += 7 * arr[0 * L1_SIZE_LINE + 1] + 4 * arr[0 * L1_SIZE_LINE + 2];	
 			break;
 		case 1:
-			arr[1 * L1_SIZE_LINE] += 4;	
+			arr[1 * L1_SIZE_LINE] += 4 * arr[1 * L1_SIZE_LINE + 1] + 4 * arr[1 * L1_SIZE_LINE + 2];	
 			break;
 		case 2:
-			arr[2 * L1_SIZE_LINE] += 11;	
+			arr[2 * L1_SIZE_LINE] += 17 * arr[2 * L1_SIZE_LINE + 1] + 4 * arr[2 * L1_SIZE_LINE + 2];	
 			break;
 		case 3:
-			arr[3 * L1_SIZE_LINE] += 1;	
+			arr[3 * L1_SIZE_LINE] += 1 * arr[3 * L1_SIZE_LINE + 1] + 4 * arr[3 * L1_SIZE_LINE + 2];	
 			break;
 		case 4:
-			arr[4 * L1_SIZE_LINE] += 27;	
+			arr[4 * L1_SIZE_LINE] += 27 * arr[4 * L1_SIZE_LINE + 1] + 4 * arr[4 * L1_SIZE_LINE + 2];	
 			break;
 		case 5:
-			arr[5 * L1_SIZE_LINE] += 24;	
+			arr[5 * L1_SIZE_LINE] += 24 * arr[5 * L1_SIZE_LINE + 1] + 4 * arr[5 * L1_SIZE_LINE + 2];	
 			break;
 		case 6:
-			arr[6 * L1_SIZE_LINE] += 21;	
+			arr[6 * L1_SIZE_LINE] += 21 * arr[6 * L1_SIZE_LINE + 1] + 4 * arr[6 * L1_SIZE_LINE + 2];	
 			break;
 		case 7:
-			arr[7 * L1_SIZE_LINE] += 221;	
+			arr[7 * L1_SIZE_LINE] += 221 * arr[7 * L1_SIZE_LINE + 1] + 4 * arr[7 * L1_SIZE_LINE + 2];	
 			break;
 
 		}	
-/*		arr[0 * L1_SIZE_LINE] = a0;
-		arr[1 * L1_SIZE_LINE] = a1;
-		arr[2 * L1_SIZE_LINE] = a2;
-		arr[3 * L1_SIZE_LINE] = a3;
-*/	}
+
+	}
+	unsigned long long end = rdtsc();
+	printf("%llu \n", (end - start) / N);
 	int res = 0;
 	for(int i = 0; i < REG_COUNT_TEST; i++) {
 		res += arr[i * L1_SIZE_LINE];
 	}
 	return res;
 }
+
 
